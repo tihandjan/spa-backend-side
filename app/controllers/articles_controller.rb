f310b91fc4293d357dcfa63509b04da5e9651fc9
tihandjan <tihandjan198921@gmail.com>
2017-02-12 17:36:28 +0300

    tiny changes in controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.where("lower(title) = ?", (params[:id]).split('-').join(' ')).first
+      @article = Article.find(params[:id].split('-')[0])
     end
 
     # Only allow a trusted parameter "white list" through.
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,7 +41,7 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.where("lower(title) = ?", (params[:id]).split('-').join(' ')).first
+      @project = Project.find(params[:id].split('-')[0])
     end
 
     # Only allow a trusted parameter "white list" through.
