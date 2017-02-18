da38f20e7af593b9eeff858630ca62038a811f2a
tihandjan <tihandjan198921@gmail.com>
2017-02-18 15:27:07 +0300

    add helper method to rspec/rails_helper

--- spec/requests/api/articles_spec.rb
+++ spec/requests/api/articles_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Articles API", type: :request do
+    let!(:article) { FactoryGirl.create_list(:article, 3) }
+    
     it 'sends public articles' do
-        article = FactoryGirl.create_list(:article, 3)
         get '/api/articles', nil, { 'Content-type': 'application/json' }
 
         expect(response.status).to eq(200)
-        json = JSON.parse(response.body)
 
         expect(json.count).to eq(3)
         expect(json[0]['title']).to eq(article.first.title)
+    end
+
+    it 'sends particular article' do
+        get "/api/articles/#{article.first.id}", nil, { 'Content-type': 'application/json' }
+
+        expect(response.status).to eq(200)
+        expect(json['title']).to eq(article.first.title)
     end
 end
